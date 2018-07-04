2211062d05f94e526988bcdcf96e2b072c3784a8
Chisholm <ChisholmE@ENT.dfo-mpo.ca>
2018-07-04 16:24:13 -0300

    asP01 function
    for converting gf3 codes to P01

---
+++ asP01.R
@@ -0,0 +1,34 @@
+####GF3 2 P01####
+
+as.P01 <- function(gf3){
+  gf32p01 <- read.csv('c:/Users/ChisholmE/Documents/sample files/GF3 Code Map.csv', header = TRUE)
+  
+ 
+  
+  line <- grep(gf32p01$GF3.code, pattern = gf3)
+  
+  if (length(line) == 0){
+    yn <- list()
+    for (i in 1:length(gf32p01$GF3.code)){
+      yn[[i]] <- grep( pattern = gf32p01$GF3.code[[i]], x = gf3, value = TRUE)
+      if(length(yn[[i]] != 0)){
+        line <- i
+      }
+    }
+    
+  }
+  if (length(line) == 0){
+    warning(paste(gf3, 'not recognized in list of GF3 codes!'))
+    stop()
+  }
+  
+  gf3 <- list(gf3 = gf3)
+  gf3$P01 <- as.character(gf32p01$P01.code[[line]])
+  gf3$P01name <-as.character(gf32p01$P01..preferred.name[[line]])
+  gf3$P06 <- as.character(gf32p01$P06.unit.code[[line]])
+  gf3$P06name <- as.character(gf32p01$P06.unit.name[[line]])
+  gf3$units <- as.character(gf32p01$units[[line]])
+  gf3$std <- as.character(gf32p01$standard_name[[line]])
+  
+  return(gf3)
+}
