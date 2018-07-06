549a6e6de5ac04675e20aa3a33c9020bda595d51
Chisholm <ChisholmE@ENT.dfo-mpo.ca>
2018-07-06 11:38:47 -0300

    efficient write out to nc

--- ctd_nc.R
+++ ctd_nc.R
@@ -29,43 +29,7 @@ source('asP01.R')
 
 ctd_nc <- function(obj, upcast = NULL, metadata, filename = NULL, write = TRUE){
   
-  if (!missing(metadata)) {
-    metad <- read.csv(metadata, header = TRUE)
-    
-    mn <- as.character(metad[,1])
-    mv <- as.character(metad[,2])
-    
-    
-    md <- as.list(mv)
-    names(md) <- mn
-  }
-  
-  ###input variables into spreadsheet###
-  # #check which variables are included
-  # lines <-  grep(names(md), pattern = 'var')
-  # 
-  # vr <- NULL
-  # for( l in lines){
-  #   vr[[l]] <- md[[l]]
-  #   }
-  # v <- NULL
-  # for( n in 1:length(lines)){
-  #   v[[n]] <- vr[[lines[[n]]]]
-  # }
-  # 
-  # numvar <- length(lines)
-  # 
-  # 
-  # ####checks
-  # 
-  # for( n in 1:length(v)){
-  #  yn <-  v[[n]] %in%(names(obj@data))
-  #     if (yn == FALSE){
-  #       warning(paste(v[[n]], "  not recognized in chosen CTD data!"))
-  #       stop()
-  #     }
-  # }
-  ##OR##
+ 
   #input varaibles automatically from obj@data
   
   v <- names(obj@data)
@@ -98,179 +62,13 @@ ctd_nc <- function(obj, upcast = NULL, metadata, filename = NULL, write = TRUE){
     
     
   }
+  numvar <- length(var)
+  if (numvar >12){
+    warning("Maximum of 12 variables exceeded! Not all data has been exported!")
+  }
+  numflag <- length(var) #FIXME: change so that function can handle more flags than variables
   
-  # #11 variables
-  # ####ALWAYS INCLUDED####
-  # variable_1 <- 'temperature'
-  # var1 <- obj@metadata$dataNamesOriginal[[variable_1]]
-  # units1 <- 'Degrees Celsius'
-  # P01_VAR1 <- 'SDN:P01::TEMPPR01'
-  # P01_name_var1 <- 'Temperature of the water body'
-  # P06_var1 <- 'SDN:P06::UPAA'
-  # P06_name_var1 <- 'Degrees Celsius'
-   std_variable_1 <- 'sea_water_temperature'
-  # var1max <- 100
-  # var1min <- -100
-  # 
-  # var1_QC <- 'TEMP_QC'
-  # variable1_QC <- paste('quality flag for', variable_1, sep = ' ')
-  # 
-  # 
-  # 
-  # variable_2 <- 'conductivity'
-  # var2 <- obj@metadata$dataNamesOriginal[[variable_2]]
-  # units2 <- 'S/m'
-  # P01_VAR2 <- 'SDN:P01::CNDCST01'
-  # P01_name_var2 <- 'Electrical conductivity of the water body by CTD'
-  # P06_var2 <- 'SDN:P06::UUUU'
-  # P06_name_var2 <- 'Dimensionless'
-   std_variable_2 <- 'sea_water_electrical_conductivity'
-  # var2max <- 1000
-  # var2min <- -1000
-  # 
-  # var2_QC <- 'CRAT_QC'
-  # variable2_QC <- paste('quality flag for', variable_2, sep = ' ')
-  # 
-  # 
-  # variable_3 <- 'pressure'
-  # var3 <- obj@metadata$dataNamesOriginal[[variable_3]]
-  # units3 <- 'decibars'
-  # P01_VAR3 <- 'SDN:P01::PRESPR01'
-  # P01_name_var3 <- 'Pressure (spatial co-ordinate) exerted by the water body by profiling pressure sensor and corrected to read zero at sea level'
-  # P06_var3 <- 'SDN:P06::UPDB'
-  # P06_name_var3 <- 'Decibars'
-   std_variable_3 <- 'sea_water_pressure'
-  # var3max <- 1000
-  # var3min <- -1000
-  # 
-  # var3_QC <- 'PRES_QC'
-  # variable3_QC <- paste('quality flag for', variable_3, sep = ' ')
-  # 
-  # ####VARIABLES####
-  # variable_4 <- 'sigmaTheta'
-  # var4 <- obj@metadata$dataNamesOriginal[[variable_4]]
-  # units4 <- 'kg/m^3'
-  # P01_VAR4 <- 'SDN:P01::SIGTPR01'
-  # P01_name_var4 <- 'Sigma-theta of the water body by CTD and computation from salinity and potential temperature using UNESCO algorithm'
-  # P06_var4 <- 'SDN:P06::UKMC'
-  # P06_name_var4 <- 'Kilograms per cubic metre'
-   std_variable_4 <- 'sea_water_sigma_theta'
-  # var4max <- 1000
-  # var4min <- -1000
-  # 
-  # var4_QC <- 'SIGP_QC'
-  # variable4_QC <- paste('quality flag for', variable_4, sep = ' ')
-  # 
-  # 
-  # 
-  # variable_5 <- 'scan'
-  # var5 <- obj@metadata$dataNamesOriginal[[variable_5]]
-  # units5 <- 'counts'
-  # P01_VAR5 <- 'SDN:P01::'
-  # P01_name_var5 <- ''
-  # P06_var5 <- 'SDN:P06::'
-  # P06_name_var5 <- ''
-  std_variable_5 <- NULL
-  # var5max <- 1000000
-  # var5min <- -1000000
-  # 
-  # var5_QC <- 'CNTR_QC'
-  # variable5_QC <- paste('quality flag for', variable_5, sep = ' ')
-  # 
-  # 
-  # variable_6 <- 'oxygen'
-  # var6 <- obj@metadata$dataNamesOriginal[[variable_6]]
-  # units6 <- 'ml/L'
-  # P01_VAR6 <- 'SDN:P01::DOXYCZ01'
-  # P01_name_var6 <- 'Concentration of oxygen {O2 CAS 7782-44-7} per unit volume of the water body [dissolved plus reactive particulate phase] by in-situ sensor and calibration against sample data'
-  # P06_var6 <- 'SDN:P06::UMLL'
-  # P06_name_var6 <- '	Millilitres per litre'
-  std_variable_6 <- NULL
-  # var6max <- 1000
-  # var6min <- -1000
-  # 
-  # var6_QC <- 'DOXY_QC'
-  # variable6_QC <- paste('quality flag for', variable_6, sep = ' ')
-  # 
-  # 
-  # 
-  # variable_7 <- 'salinity'
-  # var7 <- obj@metadata$dataNamesOriginal[[variable_7]]
-  # units7 <- '1'
-  # P01_VAR7 <- 'SDN:P01::PSLTZZ01'
-  # P01_name_var7 <- 'Practical salinity of the water body'
-  # P06_var7 <- 'SDN:P06::UUUU'
-  # P06_name_var7 <- 'Dimensionless'
-   std_variable_7 <- 'sea_water_practical_salinity'
-  # var7max <- 1000
-  # var7min <- -1000
-  # 
-  # var7_QC <- 'PSAL_QC'
-  # variable7_QC <- paste('quality flag for', variable_7, sep = ' ')
-  # 
-  # 
-  # variable_8 <- 'oxygenVoltage'
-  # var8 <- obj@metadata$dataNamesOriginal[[variable_8]]
-  # units8 <- 'V'
-  # P01_VAR8 <- 'SDN:P01::OXYOCPVL'
-  # P01_name_var8 <- 'Voltage of instrument output by oxygen sensor'
-  # P06_var8 <- 'SDN:P06::UVLT'
-  # P06_name_var8 <- 'Volts'
-   std_variable_8 <- NULL
-  # var8max <- 1000
-  # var8min <- -1000
-  # 
-  # var8_QC <- 'OXYV_QC'
-  # variable8_QC <- paste('quality flag for', variable_8, sep = ' ')
-  # 
-  # 
-  # variable_9 <- 'fluorometer'
-  # var9 <- obj@metadata$dataNamesOriginal[[variable_9]]
-  # units9 <- 'mg/m^3'
-  # P01_VAR9 <- 'SDN:P01::FLUOZZZZ'
-  # P01_name_var9 <- 'Fluorescence of the water body'
-  # P06_var9 <- 'SDN:P06::UMMC'
-  # P06_name_var9 <- 'Milligrams per cubic metre'
-   std_variable_9 <- NULL
-  # var9max <- 1000
-  # var9min <- -1000
-  # 
-  # var9_QC <- 'FLOR_QC'
-  # variable9_QC <- paste('quality flag for', variable_9, sep = ' ')
-  # 
-  # 
-  # variable_10 <- 'par'
-  # var10 <- "umol/m^2/s"
-  # units10 <- ''
-  # P01_VAR10 <- 'SDN:P01::'
-  # P01_name_var10 <- ''
-  # P06_var10 <- 'SDN:P06::'
-  # P06_name_var10 <- ''
-   std_variable_10 <- 'downwelling_photosynthetic_radiance_in_sea_water'
-  # var10max <- 1000
-  # var10min <- -1000
-  # 
-  # var10_QC <- 'PSAR_QC'
-  # variable10_QC <- paste('quality flag for', variable_10, sep = ' ')
-  # 
-  # 
-  # variable_11 <- 'scan2'
-  # var11 <- obj@metadata$dataNamesOriginal[[variable_11]]
-  # units11 <- 'counts'
-  # P01_VAR11 <- 'SDN:P01::'
-  # P01_name_var11 <- ''
-  # P06_var11 <- 'SDN:P06::'
-  # P06_name_var11 <- ''
-   std_variable_11 <- NULL
-  # var11max <- 1000
-  # var11min <- -1000
-  # 
-  # var11_QC <- 'CNTR_02_QC'
-  # variable11_QC <- paste('quality flag for', variable_11, sep = ' ')
-  # 
-  # 
   
-   
    ####combine up and down cast####
    if (!is.null(upcast)) {
      if (obj[['startTime']] == upcast[['startTime']]) {
@@ -287,7 +85,7 @@ ctd_nc <- function(obj, upcast = NULL, metadata, filename = NULL, write = TRUE){
        # }
        
      } else{
-       warning('UPCAST START TIME DOES NOT MATCH DOWNCAST START TIME!')
+       warning('UPCAST START TIME DOES NOT MATCH DOWNCAST START TIME! DOUBLE CHECK FILES!')
        stop()
      }
    }
@@ -321,107 +119,146 @@ ctd_nc <- function(obj, upcast = NULL, metadata, filename = NULL, write = TRUE){
   dlname <- variable_1
   v1_def <- ncvar_def(var1, units1, list(presdim, stationdim), FillValue, dlname, prec = 'double')
   
-  dlname <- variable1_QC
-  v1qc_def <- ncvar_def(var1_QC, units = '', list(presdim, stationdim), missval = 0, dlname, prec = 'integer')
   
+  if (numvar >1){
   dlname <- variable_2
   v2_def <- ncvar_def(var2, units2, list(presdim, stationdim), FillValue, dlname, prec = 'double')
   
-  dlname <- variable2_QC
-  v2qc_def <- ncvar_def(var2_QC, units = '', list(presdim, stationdim), missval = 0, dlname, prec = 'integer')
   
+  if (numvar >2){
   dlname <- variable_3
   v3_def <- ncvar_def(var3, units3, list(presdim, stationdim), FillValue, dlname, prec = 'double')
   
-  dlname <- variable3_QC
-  v3qc_def <- ncvar_def(var3_QC, units = '', list(presdim, stationdim), missval = 0, dlname, prec = 'integer')
   
-  
+  if (numvar >3){
   dlname <- variable_4
   v4_def <- ncvar_def(var4, units4, list(presdim, stationdim), FillValue, dlname, prec = 'double')
   
-  dlname <- variable4_QC
-  v4qc_def <- ncvar_def(var4_QC, units = '', list(presdim, stationdim), missval = 0, dlname, prec = 'integer')
-  
+ 
+  if (numvar >4){
   dlname <- variable_5
   v5_def <- ncvar_def(var5, units5, list(presdim, stationdim), FillValue, dlname, prec = 'double')
   
-  dlname <- variable5_QC
-  v5qc_def <- ncvar_def(var5_QC, units = '', list(presdim, stationdim), missval = 0, dlname, prec = 'integer')
   
+  if (numvar >5){
   dlname <- variable_6
   v6_def <- ncvar_def(var6, units6, list(presdim, stationdim), FillValue, dlname, prec = 'double')
   
-  dlname <- variable6_QC
-  v6qc_def <- ncvar_def(var6_QC, units = '', list(presdim, stationdim), missval = 0, dlname, prec = 'integer')
   
+  if (numvar >6){
   dlname <- variable_7
   v7_def <- ncvar_def(var7, units7, list(presdim, stationdim), FillValue, dlname, prec = 'double')
   
-  dlname <- variable7_QC
-  v7qc_def <- ncvar_def(var7_QC, units = '', list(presdim, stationdim), missval = 0, dlname, prec = 'integer')
   
+  if (numvar >7){
   dlname <- variable_8
   v8_def <- ncvar_def(var8, units8, list(presdim, stationdim), FillValue, dlname, prec = 'double')
   
-  dlname <- variable8_QC
-  v8qc_def <- ncvar_def(var8_QC, units = '', list(presdim, stationdim), missval = 0, dlname, prec = 'integer')
   
+  if (numvar > 8 ){
   dlname <- variable_9
   v9_def <- ncvar_def(var9, units9, list(presdim, stationdim), FillValue, dlname, prec = 'double')
   
-  dlname <- variable9_QC
-  v9qc_def <- ncvar_def(var9_QC, units = '', list(presdim, stationdim), missval = 0, dlname, prec = 'integer')
   
+  if (numvar > 9 ){
   dlname <- variable_10
   v10_def <- ncvar_def(var10, units10, list(presdim, stationdim), FillValue, dlname, prec = 'double')
   
-  dlname <- variable10_QC
-  v10qc_def <- ncvar_def(var10_QC, units = '', list(presdim, stationdim), missval = 0, dlname, prec = 'integer')
   
+  if (numvar >10){
   dlname <- variable_11
   v11_def <- ncvar_def(var11, units11, list(presdim, stationdim), FillValue, dlname, prec = 'double')
   
+  if (numvar >11){
+    dlname <- variable_12
+    v12_def <- ncvar_def(var12, units12, list(presdim, stationdim), FillValue, dlname, prec = 'double')
+    
+    
+  }
+  
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  
+  
+  dlname <- variable1_QC
+  v1qc_def <- ncvar_def(var1_QC, units = '', list(presdim, stationdim), missval = 0, dlname, prec = 'integer')
+  
+  if (numflag > 1){
+  dlname <- variable2_QC
+  v2qc_def <- ncvar_def(var2_QC, units = '', list(presdim, stationdim), missval = 0, dlname, prec = 'integer')
+  
+  if (numflag >2){
+  dlname <- variable3_QC
+  v3qc_def <- ncvar_def(var3_QC, units = '', list(presdim, stationdim), missval = 0, dlname, prec = 'integer')
+  
+  if (numflag >3){
+  dlname <- variable4_QC
+  v4qc_def <- ncvar_def(var4_QC, units = '', list(presdim, stationdim), missval = 0, dlname, prec = 'integer')
+  
+  if (numflag >4){
+  dlname <- variable5_QC
+  v5qc_def <- ncvar_def(var5_QC, units = '', list(presdim, stationdim), missval = 0, dlname, prec = 'integer')
+  
+  if (numflag >5){
+  dlname <- variable6_QC
+  v6qc_def <- ncvar_def(var6_QC, units = '', list(presdim, stationdim), missval = 0, dlname, prec = 'integer')
+  
+  if (numflag >6){
+  dlname <- variable7_QC
+  v7qc_def <- ncvar_def(var7_QC, units = '', list(presdim, stationdim), missval = 0, dlname, prec = 'integer')
+  
+  if (numflag >7){
+  dlname <- variable8_QC
+  v8qc_def <- ncvar_def(var8_QC, units = '', list(presdim, stationdim), missval = 0, dlname, prec = 'integer')
+  
+  if (numflag > 8){
+  dlname <- variable9_QC
+  v9qc_def <- ncvar_def(var9_QC, units = '', list(presdim, stationdim), missval = 0, dlname, prec = 'integer')
+  
+  if (numflag >9){
+  dlname <- variable10_QC
+  v10qc_def <- ncvar_def(var10_QC, units = '', list(presdim, stationdim), missval = 0, dlname, prec = 'integer')
+  
+  if (numflag >10){
   dlname <- variable11_QC
   v11qc_def <- ncvar_def(var11_QC, units = '', list(presdim, stationdim), missval = 0, dlname, prec = 'integer')
   
-  
-  
+  if (numflag >11){
+    dlname <- variable12_QC
+    v12qc_def <- ncvar_def(var12_QC, units = '', list(presdim, stationdim), missval = 0, dlname, prec = 'integer')
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
+  }
   
   
   #####write out definitions to new nc file####
-
+defs <- grep(ls(), pattern = '_def', value = TRUE)
+  dd <- NULL
+  for ( i in 1:length(defs)){
+    eval(parse(text = paste0("dd[[i]] <- ", defs[[i]])))
+  }
   ncout <-
     nc_create(
       ncfname,
-      list(
-        
-        lon_def,
-        lat_def,
-        
-        v1_def,
-        v2_def,
-        v3_def,
-        v4_def,
-        v5_def,
-        v6_def,
-        v7_def,
-        v8_def,
-        v9_def,
-        v10_def,
-        v11_def,
-        v1qc_def,
-        v2qc_def,
-        v3qc_def,
-        v4qc_def,
-        v5qc_def,
-        v6qc_def,
-        v7qc_def,
-        v8qc_def,
-        v9qc_def,
-        v10qc_def,
-        v11qc_def
-      ),
+      
+        dd
+      ,
       force_v4 = TRUE
     )
   
@@ -812,10 +649,20 @@ ctd_nc <- function(obj, upcast = NULL, metadata, filename = NULL, write = TRUE){
   
   #avoid printing variables as metadata
  
+  if (!missing(metadata)) {
+    metad <- read.csv(metadata, header = TRUE)
     
+    mn <- as.character(metad[,1])
+    mv <- as.character(metad[,2])
+    
+    
+    md <- as.list(mv)
+    names(md) <- mn
+  
     for (m in seq_along(md)) {
       ncatt_put(ncout, 0, names(md)[m], md[[m]])
     }
+  }
   
   ####preserve ODF history header####
   if (!is.null(obj@metadata$header)){
@@ -858,6 +705,9 @@ ctd_nc <- function(obj, upcast = NULL, metadata, filename = NULL, write = TRUE){
     
     }
   }
+  
+  
+  
   
   ####nc close####
   if (write == TRUE){
