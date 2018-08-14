eae3cb0228293390924b9b7844a1b96133fae52e
Chisholm <ChisholmE@ENT.dfo-mpo.ca>
2018-08-14 14:14:06 -0300

    bug fix in as.P01

--- ncTemplates/R/asP01.R
+++ ncTemplates/R/asP01.R
@@ -1,7 +1,7 @@
 ####GF3 2 P01####
 
 #' GF# to P01
-#' 
+#'
 #' Use this function to map gf3 codes to P01 codes for exporting to netCDF
 #'
 #' @param gf3 a gf3 standard code paramater
@@ -12,11 +12,11 @@
 #' @examples
 as.P01 <- function(gf3){
   gf32p01 <- read.csv('c:/Users/ChisholmE/Documents/sample files/GF3 Code Map.csv', header = TRUE)
-  
- 
-  
+
+
+
   line <- grep(gf32p01$GF3.code, pattern = gf3)
-  
+
   if (length(line) == 0){
     yn <- list()
     for (i in 1:length(gf32p01$GF3.code)){
@@ -25,13 +25,17 @@ as.P01 <- function(gf3){
         line <- i
       }
     }
-    
+
   }
   if (length(line) == 0){
     warning(paste(gf3, 'not recognized in list of GF3 codes!'))
-    stop()
+    return(gf3)
   }
-  
+
+  if (length(line) > 1){
+    line <- line[1]
+  }
+
   gf3 <- list(gf3 = gf3)
   gf3$P01 <- as.character(gf32p01$P01.code[[line]])
   gf3$P01name <-as.character(gf32p01$P01..preferred.name[[line]])
@@ -42,6 +46,6 @@ as.P01 <- function(gf3){
     if ( gf3$std == ""){
       gf3$std <- NULL
     }
-  
+
   return(gf3)
 }
